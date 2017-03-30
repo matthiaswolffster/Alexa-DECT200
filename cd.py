c01307f662f15b1cd602b460e14c71e5ca9315c4
Matthias Wolff <matthiaswolffster@gmail.com>
2017-03-30 18:18:33 +0200

    cd.py new usage: cd.py <jsondevicesfilename>

--- cd.py
+++ cd.py
@@ -2,15 +2,20 @@
 import json
 from pprint import pprint
 import requests
+import sys
 
-json_file='beauty.ha-devices.mwo.json'
+if len(sys.argv) <> 2:
+  print 'Usage: cd.py <inputfile>'
+  sys.exit(0)
+
+json_file=sys.argv[1]
 
 json_devices=open(json_file)
 devices = json.load(json_devices)
 json_devices.close()
 
 for device in devices:
-  print "adding ", device['name']
+  print "adding ", device['name'], " to ha-bridge"
   url='http://127.0.0.1/api/devices'
   r = requests.post(url,json.dumps(device))
   print(r.json())
